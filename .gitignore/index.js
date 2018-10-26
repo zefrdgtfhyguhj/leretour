e27a3653dd43ffad1aeea6d5d56679328dda1589
tentativeCEA <44475194+tentativeCEA@users.noreply.github.com>
2018-10-26 21:59:54 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -42,7 +42,62 @@ bot.on('message', message => {
             if (err) console.error(err)
         })
     }
+        if ( message.content === prefix+"lesquen"){
 
+        var replys = ["https://cdn.discordapp.com/attachments/505094448963911695/505467243170234378/lesquen1.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467249746903055/lesquen2.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467272953856000/lesquen3.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467280344219648/lesquen4.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467300493787136/lesquen5.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467309729513502/lesquen6.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467346211831818/lesquen7.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467355086716949/lesquen8.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467362812887090/lesquen9.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467369263595540/lesquen10.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467378906169364/lesquen11.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467386011320321/lesquen12.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467393611661313/lesquen13.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467399772962827/lesquen14.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467410954846248/lesquen15.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467432031223808/lesquen17.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467415346413570/lesquen16.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467455708200987/lesquen18.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467483453390848/lesquen20.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467487664472091/lesquen19.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467493611995157/lesquen21.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467500369149962/lesquen22.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467505716887553/lesquen23.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467511911874560/lesquen24.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467519323340800/lesquen25.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467525077925890/lesquen26.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467530358554640/lesquen27.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467538746900520/lesquen28.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467543482531851/lesquen29.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467551615287296/lesquen30.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467556480548864/lesquen31.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467561505193984/lesquen32.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467565905149982/lesquen33.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467581013032980/lesquen35.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467590030786560/lesquen36.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467591935131648/lesquen34.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467595227398154/lesquen37.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467599572697101/lesquen38.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467605742518282/lesquen39.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467611539177492/lesquen40.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467624134803456/lesquen41.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467624897904661/lesquen42.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467625980166145/lesquen43.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467634398003220/lesquen44.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467650944532493/lesquen45.png",
+        "https://cdn.discordapp.com/attachments/505094448963911695/505467664253190164/lesquen46.png",       
+    ]
+        let reponse = (replys[Math.floor(Math.random() * replys.length)])
+        var embed = new Discord.RichEmbed()
+        .setTitle("Lesquen")
+        .setImage(reponse)
+        .setColor(0x200ef5)
+    message.channel.send(embed)
+        }
     if (message.content === prefix + "aide"){
         var help = new Discord.RichEmbed()
         .setTitle("$aide")
